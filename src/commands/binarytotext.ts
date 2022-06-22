533bb6569d88c8d4c1f29453ba670d4771000d7d
Frigyes06 <eszfrigyes06@gmail.com>
2022-06-22 22:41:28 +0200

    fixed some type stuff

--- src/commands/binarytotext.ts
+++ src/commands/binarytotext.ts
@@ -10,8 +10,8 @@ export const binarytotext: CommandInterface = {
                 .setDescription("Binary code to convert.")
                 .setRequired(true)),
     run: async (interaction) => {
-        let to_convert : String | null = interaction.options.getString("binary");
-        if(to_convert == null) return;
+        let to_convert : string | null = interaction.options.getString("binary");
+        if(to_convert === null) return;
         let text = to_convert.split(" ").map(elem => String.fromCharCode(parseInt(elem, 2))).join("");
         await interaction.reply(text);
     },
