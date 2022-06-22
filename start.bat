d86e3d4ebddab43b8f93d494ba9295b436228db0
Frigyes06 <eszfrigyes06@gmail.com>
2022-06-22 16:01:10 +0200

    Ported and improved starting batch file

---
+++ start.bat
@@ -0,0 +1,50 @@
+@echo off
+:menu
+cls
+echo ==================================
+echo :   RetroJBOT Runtime Manager    :
+echo ==================================
+echo v1.0
+echo What would you like to do? (use mouse or keyboard)
+cmdMenuSel f870 "1. First-time startup" "2. Start RetroJBOT" "3. Exit"
+if %ERRORLEVEL% == 1 goto fts
+if %ERRORLEVEL% == 2 goto start
+if %ERRORLEVEL% == 3 exit
+
+:fts
+echo Starting first-time startup
+call npm install
+call npm run build
+echo Finished!
+call cmdMenuSel f870 "1. Menu" "2. Exit"
+if %ERRORLEVEL% == 1 goto menu
+if %ERRORLEVEL% == 2 exit
+
+:start
+echo Enable Loop Mode Or halt when crash?
+
+cmdMenuSel f870 "1. loop mode (recommended)" "2. Halt when crash" "3. Cancel"
+if %ERRORLEVEL% == 1 goto loop
+if %ERRORLEVEL% == 2 goto run
+if %ERRORLEVEL% == 3 goto menu
+
+:run
+cls
+echo Running Node...
+call npm ci
+call npm run build
+call npm run start
+call cmdMenuSel f870 "1. Restart" "2. Exit"
+if %ERRORLEVEL% == 1 goto run
+if %ERRORLEVEL% == 2 exit
+
+:settings
+settings.bat
+
+:loop
+cls
+call npm ci
+call npm run build
+call npm run start
+echo Restarting...
+goto loop
