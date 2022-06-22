62bade6fc57d8fd4db2cd88d5de6eb5737ba07d0
Frigyes06 <eszfrigyes06@gmail.com>
2022-06-22 16:16:20 +0200

    Added some ASCII art and version number

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,7 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import { Client } from "discord.js";
+import chalk from "chalk";
 import { CommandList } from "../commands/_CommandList.js";
 
 export const onReady = async (RetroJBOT: Client) => {
@@ -15,5 +16,5 @@ export const onReady = async (RetroJBOT: Client) => {
     { body: commandData }
   );
 
-  console.log("Discord ready!");
+  console.log(chalk.green("Discord ready!"));
 };
