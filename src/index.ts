0baf0ef751d2107b705d8e40b4cbe5b2dd387fd4
Frigyes06 <eszfrigyes06@gmail.com>
2022-06-22 16:15:46 +0200

    added some ASCII art and version number

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { Client } from "discord.js";
+import { readFileSync } from "fs";
 import { validateEnv } from "./utils/validateEnv.js";
-import {onInteraction} from "./events/interaction.js";
-import {onReady} from "./events/ready.js"
+import { onInteraction } from "./events/interaction.js";
+import { onReady } from "./events/ready.js"
+
+let packagedata = readFileSync('./package.json', { "encoding": "utf-8" })
+let parsed = JSON.parse(packagedata)
+let version = parsed["version"]
+
+console.log(`\
+########  ######## ######## ########   #######        ## ########   #######  ########
+##     ## ##          ##    ##     ## ##     ##       ## ##     ## ##     ##    ## 
+##     ## ##          ##    ##     ## ##     ##       ## ##     ## ##     ##    ## 
+########  ######      ##    ########  ##     ##       ## ########  ##     ##    ## 
+##   ##   ##          ##    ##   ##   ##     ## ##    ## ##     ## ##     ##    ##  
+##     ## ########    ##    ##     ##  #######   ######  ########   #######     ## 
+                            ╭─────────────────────────────╮
+                            │ Connecting to discord.js... │
+                            ╰─────────────────────────────╯
+                                    v${version}`);
 
 (async () => {
   if (!validateEnv()) return;
