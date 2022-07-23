72aaf2d8e1ab544f9629c047dcaeb137597db4aa
Jacobw0 <54043124+Jacobw0@users.noreply.github.com>
2022-07-23 13:27:45 -0700

    Create dice.ts

---
+++ src/commands/dice.ts
@@ -0,0 +1,22 @@
+import { CommandInterface } from "../interfaces/command";
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const dice: CommandInterface = {
+  data: new SlashCommandBuilder()
+    .setName("dice")
+    .setDescription("Shake the dice.")
+    .addStringOption((option) =>
+      option
+        .setName("dice")
+        .setDescription("Roll the dice")
+        .setRequired(false)
+    ),
+  run: async (interaction) => {
+    let string: String | null = interaction.options.getString("dice");
+    let text = string
+    if (text == null) { text = 6 }  
+    if (isNaN(text)) { interaction.reply("ℹ️ Not a number") }
+    await interaction.reply('🎲 '+Math.floor(Math.random() * text);
+    }
+  },
+};
