6c65e96444362ad832f2a0249455ce3f3c2eee6b
Frigyes06 <eszfrigyes06@gmail.com>
2022-07-23 22:44:40 +0200

    fixed dice command

--- src/commands/dice.ts
+++ src/commands/dice.ts
@@ -5,18 +5,16 @@ export const dice: CommandInterface = {
   data: new SlashCommandBuilder()
     .setName("dice")
     .setDescription("Shake the dice.")
-    .addStringOption((option) =>
+    .addIntegerOption((option) =>
       option
         .setName("dice")
         .setDescription("Roll the dice")
         .setRequired(false)
     ),
   run: async (interaction) => {
-    let string: String | null = interaction.options.getString("dice");
-    let text = string
+    let text: number | null = interaction.options.getInteger("dice");
     if (text == null) { text = 6 }  
     if (isNaN(text)) { interaction.reply("ℹ️ Not a number") }
-    await interaction.reply('🎲 '+Math.floor(Math.random() * text);
+    await interaction.reply('🎲 '+Math.floor(Math.random() * text));
     }
-  },
-};
+  }
