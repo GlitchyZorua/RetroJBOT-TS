2e45ff4e183d704d53880f01a991d5ddabdc2445
Frigyes06 <eszfrigyes06@gmail.com>
2022-08-03 18:21:48 +0200

    Readded and fixed useless web command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -4,6 +4,7 @@ import { binarytotext } from "./binarytotext.js";
 import { about } from "./about.js";
 import { help } from "./help.js";
 import { dice } from "./dice.js";
+import { uselessweb } from "./uselessweb.js";
 
 
-export const CommandList: CommandInterface[] = [ping, binarytotext, about, help, dice];
+export const CommandList: CommandInterface[] = [ping, binarytotext, about, help, dice, uselessweb];
