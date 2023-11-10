de107763d6597db3ab46a0b31bc3b48b52c615ba
Jacobw0 <54043124+GlitchyZorua@users.noreply.github.com>
2023-11-10 12:14:32 -0800

    dead joke

--- src/commands/donut.ts
+++ src/commands/donut.ts
@@ -4,7 +4,7 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 export const donut: CommandInterface = {
   data: new SlashCommandBuilder()
     .setName("donut")
-    .setDescription("dead joke, soon to be merged to a new tag. probably"),
+    .setDescription("dead joke"),
   run: async (interaction) => {
     await interaction.reply("**HALPPPPPPP I HAVE TOO MANY DONUTS** :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut: :doughnut:");
   },
