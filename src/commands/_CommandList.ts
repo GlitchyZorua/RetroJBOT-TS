2499e16dde764a6fe7219e4ba7e6714316197f1d
GlitchyZorua <54043124+GlitchyZorua@users.noreply.github.com>
2024-01-13 14:25:22 -0800

    FINALLY HOLY SHIT

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,19 +1,58 @@
-// this will be overloaded with commands some day... too bad.
-import { CommandInterface } from "../interfaces/command";
-import { ping } from "./ping.js";
-import { binarytotext } from "./binarytotext.js";
-import { about } from "./about.js";
-import { help } from "./help.js";
-import { dice } from "./dice.js";
-import { uselessweb } from "./uselessweb.js";
-import { time } from "./time.js";
-import { calc } from "./calc.js";
-import { eball } from "./8ball.js";
-import { uptime } from "./uptime.js";
-import { cards } from "./cards.js";
-import { donut } from "./donut.js";
-import { xn } from "./69ball.js";
-import { coinflip } from "./coinflip.js";
-import { cook } from "./cook.js";
-import { fact } from "./fact.js";
-export const CommandList: CommandInterface[] = [fact, cook, ping, about, help, uselessweb, time, calc, eball, uptime, cards, donut, xn, coinflip, binarytotext, dice];
+import { ChatInputCommandInteraction, CommandInteractionOptionResolver } from 'discord.js';
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+const talkedRecently = new Set<string>();
+const percentages = ['1', '5', '9', '10', '20', '30', '40', '50', '60', '70', '80', '90', '99'];
+
+export const cook = {
+  data: new SlashCommandBuilder()
+    .setName('cook')
+    .setDescription('Cook something.')
+    .addStringOption(option =>
+      option
+        .setName('thing')
+        .setDescription('The thing to cook.')
+        .setRequired(true),
+    ),
+  run: async (interaction: ChatInputCommandInteraction) => {
+    // Get the user who invoked the command and the thing they want to cook.
+    const user = interaction.user;
+    const thing = interaction.options.getString('thing');
+
+    // Check if the user has already cooked recently.
+    if (talkedRecently.has(user.id)) {
+      interaction.reply({
+        content: '⏰ **Cooldown** You are cooking too fast! Please slow down after 9 seconds.',
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // Add the user to the cooldown liSst.
+    talkedRecently.add(user.id);
+
+    // Remove the user from the cooldown list after 9 seconds.
+    setTimeout(() => {
+      talkedRecently.delete(user.id);
+    }, 9000);
+
+    // Send a message to the user indicating that their cooking is in progress.
+    const cookMessage = await interaction.reply({
+      content: `<a:Loading_Color:759914061696860202> **COOKING** ${thing} 0%`,
+    });
+
+    // Create an interval loop to update the cooking progress message.
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i >= percentages.length) {
+        // Finish cooking and send a success message to the user.
+        cookMessage.edit(`**DONE COOKING** Successfully cooked ${thing}!`);
+        clearInterval(interval);
+        return;
+      }
+
+      // Update the cooking progress message.
+      cookMessage.edit(`<a:Loading_Color:759914061696860202> **COOKING** ${thing} - ${percentages[i++]}%`);
+    }, 1000);
+  },
+};
