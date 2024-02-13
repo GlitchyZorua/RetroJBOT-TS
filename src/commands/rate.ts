859415ed62715a428c0e498468aae67fa6b0d573
GlitchyZorua <54043124+GlitchyZorua@users.noreply.github.com>
2024-02-12 17:41:11 -0800

    Update rate.ts

--- src/commands/rate.ts
+++ src/commands/rate.ts
@@ -13,7 +13,7 @@ export const rate = {
     ),
   async run(interaction: ChatInputCommandInteraction<CacheType>) {
     try {
-      const text = interaction.options.getString('text') ?? '';
+      const text = interaction.options.getString('thing') ?? '';
       const num = Math.floor(Math.random() * 10)
       await interaction.reply('I rate '+ text + ' ' + num + '/10');
     } catch (error) {
