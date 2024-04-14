b0a209cf7471aea89587bbfe65163bd931913ffa
GlitchyZorua <54043124+GlitchyZorua@users.noreply.github.com>
2024-04-14 12:51:47 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,29 @@
 @echo off
+:startup
+:: Arguments 
 
-:: I don't feel like touchin this code, all it does is launch the runtime
-:: I feel it get worse in the future.
-:: OH WELL.
+IF "%1"=="-?" goto help
+:: Skip Wine Checks
+IF "%1"=="--no-wine-check" goto menu
+:: First Time Setup
+IF "%1"=="-fts" goto fts
+:: loop-mode
+IF "%1"=="--loop-mode" goto loop
+:: Normal Mode
+IF "%1"=="--normal-mode" goto start
 
+goto winecheck
+:help
+echo Here is a list of arguments you can use:
+echo -? - this
+echo -fts - Runs first time setup
+echo --loop-mode - Runs Loop Mode
+echo --normal-mode - Runs normal mode
+echo --no-wine-check - Skips the wine check
+
+
+cmd
+:winecheck
 setlocal
 break /? 2>nul
 if errorlevel 1 goto wine
@@ -15,47 +35,18 @@ dpath /? 2>nul
 if errorlevel 1 goto wine
 set random=
 if not defined random goto wine
-goto :c
+goto menu
 
-:: ============================
-:: ANTI STUPID WARNINGS
-:: ============================
 
 :wine
-echo =============================================================
-echo : You are using this script in wine. Please don't do this.  :
-echo : This script is NOT MEANT TO BE USED WITH WINE. AND HAS    :
-echo : THE POTENTIAL OF BREAKING STUFF! PLEASE USE LOAD.SH       :
-echo : INSTEAD. Press any key to terminate the script.           :
-echo =============================================================
-pause
-exit
-
-
-
-:: this is a hack. 
-ver | findstr /i "5\.0\." > nul
-IF %ERRORLEVEL% EQU 0 goto notsupported
-ver | findstr /i "5\.1\." > nul
-IF %ERRORLEVEL% EQU 0 goto notsupported
-ver | findstr /i "5\.2\." > nul
-IF %ERRORLEVEL% EQU 0 goto notsupported
-ver | findstr /i "6\.0\." > nul
-IF %ERRORLEVEL% EQU 0 goto notsupported
-ver | findstr /i "6\.1\." > nul
-IF %ERRORLEVEL% EQU 0 goto notsupported
-goto c
-:: this code is untested. oh fucking well. 
-
-
-:notsupported
-echo ===============================
-echo : Your OS isn't supported.    :
-echo : This script will terminate. :
-echo ===============================
-pause
-exit
-
+echo Please don't run this batch file under wine! If you are trying to run the bot on linux, please run "start.sh" (without using wine.)
+echo If you still want to proceed, you will be excluded from reporting a issue on github, UNLESS you are seeing this message in a error.
+echo You can skip this check by adding the "--no-wine-check" argument
+ver
+set /P c=Do you still try running this? (Press A if you wanna check again) (y/n/a)
+if /I "%c%" EQU "Y" goto menu
+if /I "%c%" EQU "N" exit
+if /I "%c%" EQU "A" goto startup
 
 :: the menu
 :menu
@@ -63,7 +54,7 @@ cls
 echo ==================================
 echo :   RetroJBOT Runtime Manager    :
 echo ==================================
-echo v1.0
+echo v1.0.0
 echo What would you like to do? (use mouse or keyboard)
 cmdMenuSel f870 "1. First-time startup" "2. Start RetroJBOT" "3. Exit" 
 :: this menu was ripped from a batch os.
@@ -111,8 +102,3 @@ call npm run build
 call npm run start
 echo Restarting...
 goto loop
-
-:: BATCH + JS = SHIT
-:: If this scripts breaks due to microsoft, i'm forced to rewrite it. without using batch script
-:: probably powershell.
-:: JUST FOR A GOD DAMN LAUNCHER FOR A DISCORD BOT. FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
